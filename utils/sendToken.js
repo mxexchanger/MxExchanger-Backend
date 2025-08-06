759c830dbbfc0531ff304dd862bc3765e991eb95
fahoo <fahoo@localhost.localdomain>
2025-08-06 16:55:49 +0500

    Updated backend code with latest fixes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,4 +1,4 @@
-const sendToken = (user, statusCode, res) => {
+export const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
     const expires = new Date(Date.now() + 4 * 24 * 60 * 60 * 1000).toUTCString();
