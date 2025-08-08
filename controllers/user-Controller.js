5aaf2ce0908af9670d91e98167cc907eed2f8cf8
fahoo <fahoo@localhost.localdomain>
2025-08-08 13:45:45 +0500

    Updated profile update API

--- controllers/user-Controller.js
+++ controllers/user-Controller.js
@@ -655,10 +655,10 @@ export const isLoginUser = asyncHandler(async (req, res, next) => {
 
 
 export const logout = (req, res) => {
-    res.cookie('token', '', {
+    res.clearCookie('token', {
         httpOnly: true,
-        expires: new Date(0),
-        sameSite: 'lax',
+        sameSite: 'none',
+        secure: true
     });
 
     return res.status(200).json({
