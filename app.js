f6e9dff9a8ce6bba4b896a9139615e627f5eb623
fahoo <fahoo@localhost.localdomain>
2025-08-11 15:40:02 +0500

    Updated crose

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 // ✅ FINAL CORS CONFIG
 const corsOptions = {
     origin: ['https://mxexchanger.com', 'http://mxexchanger.com', 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true, // ✅ Enable cookies or auth headers
 };
 
